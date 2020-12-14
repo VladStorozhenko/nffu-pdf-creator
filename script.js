cbe2cca00377b33c8710ce35ec03905d043403f9
Vladislav Storozhenko <vladstorozhenkokh@gmail.com>
2020-12-14 20:37:16 +0200

    New functionality
    Nom it previews the pdf right on the page instead of downloading it

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const { jsPDF } = window.jspdf;
 
-const doc = new jsPDF();
-
 const savePDF = () => {
     const name = document.getElementById("name").value;
     const surname = document.getElementById("surname").value;
 
-    doc.setFont('Roboto-Regular')
+    doc.setFont("Roboto-Regular");
 
-    doc.text(40, 20, `Ваши имя и фамилия:`)
-    doc.text(40, 50, `${name} ${surname}`)
+    doc.text(40, 20, `Ваши имя и фамилия:`);
+    doc.text(40, 50, `${name} ${surname}`);
+    doc.output();
     doc.save("имя_фамилия.pdf");
 };
 
-document.getElementById("button-submit").addEventListener("click", () => {
-    savePDF()
+const testFunction = (e) => {
+
+    let doc = new jsPDF();
+
+    e.preventDefault();
+
+    let string = "";
+
+    const previewContainer = document.getElementById("preview-container");
+
+    previewContainer.innerHTML = "";
+
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+
+    doc.setFont("Roboto-Regular");
+
+    doc.text(40, 20, `Ваши имя и фамилия:`);
+    doc.text(40, 50, `${name} ${surname}`);
+
+    doc.output();
+
+    string = doc.output("datauristring");
+
+    var embed = "<embed width='50%' height='500px' src='" + string + "'/>";
+
+    previewContainer.insertAdjacentHTML("afterbegin", embed);
+};
+
+document.getElementById("button-submit").addEventListener("click", (e) => {
+    testFunction(e);
 });
